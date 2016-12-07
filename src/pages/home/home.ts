8868866b1ae28b56162892f46eab0828a99804c3
Diego Peralta <diego.peralta.dev@gmail.com>
2016-12-07 00:29:13 -0300

    add scan button

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -2,14 +2,39 @@ import { Component } from '@angular/core';
 
 import { NavController } from 'ionic-angular';
 
+import { BarcodeScanner } from 'ionic-native';
+
+import { Product } from '../../models/product';
+
+import { PreciosClarosProductos } from '../../providers/precios-claros-productos';
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
-    
+  products: Product[]
+
+  constructor(public navCtrl: NavController, private preciosclarosProductos: PreciosClarosProductos) { }
+
+  scan(){
+    BarcodeScanner.scan().then((barcodeData) => {
+      // Success! Barcode data is here
+      this.preciosclarosProductos.load(barcodeData.text).subscribe(products => {
+        this.products = products
+        console.log(products);
+      });
+
+      console.log(barcodeData.text);
+    }, (err) => {
+      // An error occurred
+      console.log(err);
+    });
+  }
+
+  ionViewDidLoad() {
+    console.log('Hello world');
   }
 
 }
