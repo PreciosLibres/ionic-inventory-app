8bfad7eb725eda42ad8a4b05819d5b3355b1f320
Diego Peralta <diego.peralta.dev@gmail.com>
2016-12-26 19:27:11 +0000

    Update precios-claros-productos.ts

--- src/providers/precios-claros-productos.ts
+++ src/providers/precios-claros-productos.ts
@@ -14,7 +14,7 @@ import { Product } from '../models/product';
 @Injectable()
 export class PreciosClarosProductos {
 
-  preciosClarosApiUrl =  'http://198.199.72.76/preciosclaros';
+  preciosClarosApiUrl =  'http://precioslibres.org/preciosclaros';
 
   constructor(public http: Http) {
     console.log('Hello PreciosClarosProductos Provider');
